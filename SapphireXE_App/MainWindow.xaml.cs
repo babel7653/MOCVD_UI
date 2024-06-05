c430bc072587bea783a591f5ae5e8aa4113c16ec
sysnex <babel7653@naver.com>
2024-06-05 10:36:21 +0900

    6월5일 LiveChart

--- SapphireXE_App/MainWindow.xaml.cs
+++ SapphireXE_App/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Shapes;
 using System.Drawing;
 using TwinCAT.Ads;
 using System.IO;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace SapphireXE_App
 {
@@ -25,9 +27,38 @@ namespace SapphireXE_App
     {
         //Create an instance of the the TcAdsClient
         TcAdsClient tcClient;
+        //Live Chart용 코드
+        public SeriesCollection SeriesData { get; private set; }
+        public string[] XLabel { get; set; }
+        public Func<int, string> Values { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            //Live Chart 테스트 코드
+            SeriesData = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "2020",
+                    Values = new ChartValues<int>{3,5,7,4,7 }
+                },
+                new ColumnSeries
+                {
+                    Title ="2021",
+                    Values = new ChartValues<int>{5,6,2,7,8}
+                },
+                new ColumnSeries
+                {
+                    Title = "2022",
+                    Values = new ChartValues<int> { 8, 7, 6, 9, 7}
+                }
+            };
+            XLabel = new string[] { "Kang", "Kim", "Cho", "Ko", "Song" };
+            Values = value => value.ToString("N");
+
+            DataContext = this;
+
         }
 
 

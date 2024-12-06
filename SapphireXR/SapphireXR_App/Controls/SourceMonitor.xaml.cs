f3bc1ece26452a554b94fb96203043d5662a282e
sangchul, Kim <babel7653@naver.com>
2024-12-06 19:34:39 +0900

    수정

---
+++ SapphireXR/SapphireXR_App/Controls/SourceMonitor.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SapphireXR_App.Controls
+{
+    /// <summary>
+    /// SourceMonitor.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SourceMonitor : UserControl
+    {
+        public SourceMonitor()
+        {
+            InitializeComponent();
+        }
+        public string MonitorID
+        {
+            get { return (string)GetValue(MonitorIDProperty); }
+            set { SetValue(MonitorIDProperty, value); }
+        }
+
+        public static readonly DependencyProperty MonitorIDProperty =
+            DependencyProperty.Register("MonitorID", typeof(string), typeof(SourceMonitor), new PropertyMetadata(default));
+
+        private void sourceMonitor_Loaded(object sender, RoutedEventArgs e)
+        {
+            SourceMonitor monitor = (SourceMonitor)sender;
+            sourceName.Text = monitor.MonitorID;
+        }
+    }
+}

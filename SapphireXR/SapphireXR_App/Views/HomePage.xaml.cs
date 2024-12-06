8b5678010d4537b726807844a2e8946a03d0fd26
sangchul, Kim <babel7653@naver.com>
2024-12-06 16:39:57 +0900

    Revert "수정"
    This reverts commit 564a28135a054f793c8745c41fbd3b18270251d0.

--- SapphireXR/SapphireXR_App/Views/HomePage.xaml.cs
+++ SapphireXR/SapphireXR_App/Views/HomePage.xaml.cs
@@ -19,5 +19,117 @@ namespace SapphireXR_App.Views
             InitializeComponent();
             DataContext = App.Current.Services.GetService(typeof(HomeViewModel));
         }
+
+        private void SwitchingValve_Click(object sender, MouseButtonEventArgs e)
+        {
+            SwitchingValve Valve = (SwitchingValve)((Button)e.OriginalSource).Parent;
+            if (Valve.IsOpen == true)
+            {
+                var result = ValveOperationEx.Show("Valve Operation", $"{Valve.ValveID} 밸브를 질소가스로 변경하시겠습니까?");
+                switch (result)
+                {
+                    case ValveOperationExResult.Ok:
+                        Valve.IsOpen = !(Valve.IsOpen);
+                        MessageBox.Show($"{Valve.ValveID} 밸브 닫음");
+                        break;
+                    case ValveOperationExResult.Cancel:
+                        MessageBox.Show($"{Valve.ValveID} 취소됨1");
+                        break;
+                }
+            }
+            else
+            {
+                var result = ValveOperationEx.Show("Valve Operation", $"{Valve.ValveID} 밸브를 공정가스로 변경하시겠습니까?");
+                switch (result)
+                {
+                    case ValveOperationExResult.Ok:
+                        Valve.IsOpen = !(Valve.IsOpen);
+                        MessageBox.Show($"{Valve.ValveID} 밸브 열음");
+                        break;
+                    case ValveOperationExResult.Cancel:
+                        MessageBox.Show($"{Valve.ValveID} 취소됨2");
+                        break;
+                }
+            }
+        }
+
+        private void BypassValve_Click(object sender, MouseButtonEventArgs e)
+        {
+            SwitchingValve Valve = (SwitchingValve)sender;
+            if (Valve.IsOpen == true)
+            {
+                var result = ValveOperationEx.Show("Valve Operation", $"{Valve.ValveID} 밸브를 질소가스로 변경하시겠습니까?");
+                switch (result)
+                {
+                    case ValveOperationExResult.Ok:
+                        Valve.IsOpen = !(Valve.IsOpen);
+                        MessageBox.Show($"{Valve.ValveID} 밸브 닫음");
+                        break;
+                    case ValveOperationExResult.Cancel:
+                        MessageBox.Show($"{Valve.ValveID} 취소됨1");
+                        break;
+                }
+            }
+            else
+            {
+                var result = ValveOperationEx.Show("Valve Operation", $"{Valve.ValveID} 밸브를 공정가스로 변경하시겠습니까?");
+                switch (result)
+                {
+                    case ValveOperationExResult.Ok:
+                        Valve.IsOpen = !(Valve.IsOpen);
+                        MessageBox.Show($"{Valve.ValveID} 밸브 열음");
+                        break;
+                    case ValveOperationExResult.Cancel:
+                        MessageBox.Show($"{Valve.ValveID} 취소됨2");
+                        break;
+                }
+            }
+        }
+
+        private void SingleValve_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ButterflyValve_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void LeakTestValve_Click(object sender, MouseButtonEventArgs e)
+        {
+            LeakTestValve Valve = (LeakTestValve)((Button)e.OriginalSource).Parent;
+            //LeakTestValve Valve = (LeakTestValve)sender;
+            //LeakTestValve Valve = e.Source as LeakTestValve;
+
+            if (Valve.IsOpen == true)
+            {
+                var result = ValveOperationEx.Show("Valve Operation", $"Leak Test 밸브 {Valve.ValveID}를 닫으시겠습니까?");
+                switch (result)
+                {
+                    case ValveOperationExResult.Ok:
+                        Valve.IsOpen = !(Valve.IsOpen);
+                        MessageBox.Show($"{Valve.ValveID} 밸브 닫음");
+                        break;
+                    case ValveOperationExResult.Cancel:
+                        MessageBox.Show($"{Valve.ValveID} 취소됨1");
+                        break;
+                }
+            }
+            else
+            {
+                var result = ValveOperationEx.Show("Valve Operation", $"Leak Test 밸브{Valve.ValveID} 를 열겠습니까?");
+                switch (result)
+                {
+                    case ValveOperationExResult.Ok:
+                        Valve.IsOpen = !(Valve.IsOpen);
+                        MessageBox.Show($"{Valve.ValveID} 밸브 열음");
+                        break;
+                    case ValveOperationExResult.Cancel:
+                        MessageBox.Show($"{Valve.ValveID} 취소됨2");
+                        break;
+                }
+            }
+        }
     }
 }

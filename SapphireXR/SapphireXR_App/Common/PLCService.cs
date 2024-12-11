dd964625c132ef8d82ba50101a93d056f92756b5
함석환 <@DESKTOP-0NC4CQ4>
2024-12-11 15:28:12 +0900

    PLC 입출력 관련 기능을 전용으로 담당하는 PLCService 추가

---
+++ SapphireXR/SapphireXR_App/Common/PLCService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwinCAT.Ads;
+
+namespace SapphireXR_App.Common
+{
+    static class PLCService
+    {
+        static PLCService() 
+        {
+            Ads = new AdsClient();
+            ValveIDtoOutputSolValveIdx = new Dictionary<string, uint>
+            {
+                //여기에 밸브 와 인덱스 값 매핑 정보를 넣어주세요. 
+                { "V05", 4 }
+            };
+        }
+
+        public static AdsClient Ads
+        {
+            get; set;
+        }
+
+        public static Dictionary<string, uint> ValveIDtoOutputSolValveIdx
+        {
+            get; set;
+        }
+    }
+}

315576365dae03c6f35cdc8dd82c3768ff253fd8
epsxk82 <epsxk82@naver.com>
2025-02-20 20:28:21 +0900

    1. Manual Batch: Batch 추가, 삭제 및 편집 기능 추가.

--- SapphireXR/SapphireXR_App/ViewModels/ManualBatchViewModel.cs
+++ SapphireXR/SapphireXR_App/ViewModels/ManualBatchViewModel.cs
@@ -1,23 +1,121 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using SapphireXR_App.Models;
 
 namespace SapphireXR_App.ViewModels
 {
-    internal class ManualBatchViewModel: ObservableObject
+    public partial class ManualBatchViewModel: ObservableObject
     {
-        public ICommand BatchCommand => new RelayCommand(() =>
+        public partial class IOUserState: ObservableObject
+        {
+            [ObservableProperty]
+            private string _iD = "";
+
+            [ObservableProperty]
+            private string _name = "";
+        }
+
+        public partial class AnalogIOUserState: IOUserState
+        {
+            [ObservableProperty]
+            private int _value;
+
+            [ObservableProperty]
+            private int _maxValue;
+        }
+
+        public partial class DigitalIOUserState: IOUserState
+        {
+            [ObservableProperty]
+            private bool _on;
+        }
+
+        public partial class Batch: ObservableObject
+        {
+            [ObservableProperty]
+            private string _name = "";
+
+            [ObservableProperty]
+            private int? _rampingTime = null;
+
+            [ObservableProperty]
+            private IList<AnalogIOUserState> _analogIOUserStates = new List<AnalogIOUserState>();
+
+            [ObservableProperty]
+            private IList<DigitalIOUserState> _digitalIOUserStates = new List<DigitalIOUserState>();
+        }
+
+        public ManualBatchViewModel()
+        {
+            Batches.CollectionChanged += (object? sender, NotifyCollectionChangedEventArgs e) => {
+                MinusCommand.NotifyCanExecuteChanged();
+                LoadToPLCCommand.NotifyCanExecuteChanged();
+                SaveCommand.NotifyCanExecuteChanged();
+            };
+        }
+
+        bool batchesEmpty()
+        {
+            return 0 < Batches.Count;
+        }
+
+        [RelayCommand(CanExecute = "batchesEmpty")]
+        private void LoadToPLC()
         {
 
-        });
-        public ICommand SaveCommand => new RelayCommand(() =>
+        }
+
+
+        [RelayCommand(CanExecute = "batchesEmpty")]
+        private void Save()
         {
 
+        }
+
+        public ICommand AddCommand => new RelayCommand(() =>
+        {
+            Batch newBatch = new Batch() { Name = "UserState" + (Batches.Count + 1) };
+            foreach((string flowController, int idx) in PLCService.dIndexController)
+            {
+                newBatch.AnalogIOUserStates.Add(new AnalogIOUserState() { ID = flowController, MaxValue = (int)PLCService.ReadMaxValue(flowController) });
+            }
+            foreach((string valve, int idx) in PLCService.ValveIDtoOutputSolValveIdx1)
+            {
+                newBatch.DigitalIOUserStates.Add(new DigitalIOUserState() { ID = valve });
+            }
+            foreach ((string valve, int idx) in PLCService.ValveIDtoOutputSolValveIdx2)
+            {
+                newBatch.DigitalIOUserStates.Add(new DigitalIOUserState() { ID = valve });
+            }
+            Batches.Add(newBatch);
+            CurrentBatch = newBatch;
+
         });
+
+       
+        [RelayCommand(CanExecute = "batchesEmpty")]
+        private void Minus()
+        {
+            if (CurrentBatch != null)
+            {
+                Batches.Remove(CurrentBatch);
+                CurrentBatch = null;
+            }
+        }
+
+        [ObservableProperty]
+        private ObservableCollection<Batch> _batches = new ObservableCollection<Batch>();
+
+        [ObservableProperty]
+        private Batch? _currentBatch = null;
     }
 }

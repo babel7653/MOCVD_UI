e56aef6e0494512582c5bc20bc39bb2564d16092
함석환 <epsxk82@naver.com>
2025-03-16 20:13:33 +0900

    Source Monitor: 소수줌, 정수부 포함 4자리 표시 기능 추가

--- SapphireXR/SapphireXR_App/ViewModels/PresentValueMonitor/GasMonitorViewModel.cs
+++ SapphireXR/SapphireXR_App/ViewModels/PresentValueMonitor/GasMonitorViewModel.cs
@@ -1,18 +1,6 @@
-﻿using SapphireXR_App.Common;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SapphireXR_App.ViewModels
+﻿namespace SapphireXR_App.ViewModels
 {
     public class GasMonitorViewModel: PresentValueMonitorViewModel
     {
-        protected override void updatePresentValue(float value)
-        {
-            PresentValue = value.ToString("N", new NumberFormatInfo() { NumberDecimalDigits = Util.NumberDecimalDigits(value, GlobalSetting.MaxNumberDigit) });
-        }
     }
 }
--- SapphireXR/SapphireXR_App/ViewModels/PresentValueMonitor/SourceMonitorViewModel.cs
+++ SapphireXR/SapphireXR_App/ViewModels/PresentValueMonitor/SourceMonitorViewModel.cs
@@ -1,14 +1,6 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SapphireXR_App.ViewModels
+﻿namespace SapphireXR_App.ViewModels
 {
     internal class SourceMonitorViewModel: PresentValueMonitorViewModel
     {
-        
     }
 }

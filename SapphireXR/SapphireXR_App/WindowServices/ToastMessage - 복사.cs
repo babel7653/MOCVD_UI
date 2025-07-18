e1554611b617341fb6dd590f753bc2f60bccf98a
함석환 <epsxk82@naver.com>
2025-07-18 12:14:29 +0900

    1. Recpe Run/Recpe Edit: Recipe Edit에서 recipe를 Recipe Run으로 Load 후, recipe를 시작 하면, Recipe Edit에서도 Recipe Run처럼 recipe row의 배경/폰트색 바뀌는 문제 수정 2. Report: export 시 배경색 검정계열 색으로 되게 수정\ 3. 불필요한 파일 및 style들 삭제

---
+++ SapphireXR/SapphireXR_App/WindowServices/ToastMessage - 복사.cs
@@ -0,0 +1,64 @@
+﻿//using ToastNotifications.Lifetime;
+//using ToastNotifications.Position;
+//using ToastNotifications.Messages;
+//using ToastNotifications;
+//using System.Windows;
+//using System.Windows.Threading;
+
+//namespace SapphireXR_App.WindowServices
+//{
+//    static class ToastMessage
+//    {
+//        public enum MessageType { Information = 0, Sucess, Warning, Error };
+
+//        public static void Init()
+//        {
+//            ToastMessageTask.Start();
+//            DispatcherReadyEvent.WaitOne();
+//        }
+        
+//        public static void Show(string message, MessageType messageType)
+//        {
+//            ToastMessageDispatcher?.BeginInvoke(() =>
+//            {
+//                switch (messageType)
+//                {
+//                    case MessageType.Information:
+//                        notifier.ShowInformation(message);
+//                        break;
+
+//                    case MessageType.Warning:
+//                        notifier.ShowWarning(message);
+//                        break;
+
+//                    case MessageType.Error:
+//                        notifier.ShowError(message);
+//                        break;
+
+//                    case MessageType.Sucess:
+//                        notifier.ShowSuccess(message);
+//                        break;
+//                }
+//            });
+//        }
+
+//        private static ManualResetEvent DispatcherReadyEvent = new ManualResetEvent(false);
+//        private static Thread ToastMessageTask = new Thread(() =>
+//        {
+//            notifier = new Notifier(cfg =>
+//            {
+//                cfg.PositionProvider = new WindowPositionProvider(parentWindow: Application.Current.MainWindow, corner: Corner.BottomRight, offsetX: 10, offsetY: 10);
+//                cfg.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(notificationLifetime: TimeSpan.FromSeconds(3), maximumNotificationCount: MaximumNotificationCount.FromCount(5));
+//                cfg.Dispatcher = ToastMessageDispatcher;
+//            });
+
+//            ToastMessageDispatcher = Dispatcher.CurrentDispatcher;
+//            DispatcherReadyEvent.Set();
+//            Dispatcher.Run();
+//        });
+//        private static Notifier? notifier = null;
+//        private static Dispatcher? ToastMessageDispatcher = null;
+       
+
+//    }
+//}

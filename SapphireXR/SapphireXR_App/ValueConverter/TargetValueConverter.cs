14407d7747781969c6ca02e3ecf739cb68611527
함석환 <epsxk82@naver.com>
2025-09-11 12:24:55 +0900

    1. SapphireXR_App Manual Batch: flow controller optional value 입력 기능 추가 (셀에 아무값도 주지 않으면 해당 flow cotroller는 PLC에 target value를 전달하지 않음)

---
+++ SapphireXR/SapphireXR_App/ValueConverter/TargetValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace SapphireXR_App.ValueConverter
+{
+    internal class TargetValueConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        object? IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string? targetValue = value as string;
+            if (targetValue != null)
+            {
+                if (targetValue == string.Empty)
+                {
+                    return null;
+                }
+                else
+                {
+                    return value;
+                }
+            }
+            else
+            {
+                return Binding.DoNothing;
+            }
+        }
+    }
+}

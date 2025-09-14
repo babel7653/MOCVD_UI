6368c857eed0bb4ca1bfecc2e21894b4dc3a60a2
함석환 <epsxk82@naver.com>
2025-09-14 18:09:23 +0900

    1. SappireXR_App: MOSourceSetting 설정 기능 중간 커밋

--- SapphireXR/SapphireXR_App/ViewModels/PresentValueMonitor/SourceMonitorViewModel.cs
+++ SapphireXR/SapphireXR_App/ViewModels/PresentValueMonitor/SourceMonitorViewModel.cs
@@ -1,6 +1,20 @@
-﻿namespace SapphireXR_App.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace SapphireXR_App.ViewModels
 {
-    internal class SourceMonitorViewModel: PresentValueMonitorViewModel
+    internal partial class SourceMonitorViewModel: PresentValueMonitorViewModel
     {
+        private static readonly Brush DefaultSourceMonitorColorOnMouseOver = (DesignerProperties.GetIsInDesignMode(new DependencyObject()) == false ? App.Current.Resources.MergedDictionaries[0]["SourceMonitorColorOnMouseOver"] as Brush : null) ?? new SolidColorBrush(Color.FromRgb(0x9d, 0xbc, 0xe8));
+
+        [ObservableProperty]
+        private Brush onMouseOverBackground = DefaultSourceMonitorColorOnMouseOver;
+
+        [RelayCommand]
+        private void MouseLeftButtonDown()
+        { }
     }
 }
